5f71b4d9e79dec19dde8897254371c715dd82f79
Oscar Gonzalez Lesaga <oscar.gonzalez.lesaga@everis.com>
2018-05-14 11:59:29 +0200

    add service for token refresh FCM

---
+++ app/src/main/java/com/everis/firebaseandroid/MyFirebaseInstanceIDService.java
@@ -0,0 +1,11 @@
+package com.everis.firebaseandroid;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    
+    @Override
+    public void onTokenRefresh() {
+
+    }
+}
